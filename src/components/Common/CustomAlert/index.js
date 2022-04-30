06128406a3a1a05b572e72cd14628431c2b2ab08
Javiera Fortunato <jfortunato@moveapps.cl>
2022-04-30 16:27:42 -0400

    Ultimos arreglos de estilo menores

--- src/components/Common/CustomAlert/index.js
+++ src/components/Common/CustomAlert/index.js
@@ -2,7 +2,7 @@ import { Alert } from "react-bootstrap";
 
 const CustomAlert = ({ text, children, variant, ...restOfProps }) => {
   return (
-    <div style={{ position: "absolute", top: '30vh' }}>
+    <div style={{ position: "fixed", top: '6vh' }}>
       <Alert variant={variant} {...restOfProps}>
        {children ? children : text} 
       </Alert>
